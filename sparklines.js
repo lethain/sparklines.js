66c69296cd29b0f332e4dc589145e511ea76d791
Will Larson <lethain@gmail.com>
2008-06-30 22:33:19 +0900

    Added a prototype of Array.map for Internet Explorer compatability.

--- sparklines.js
+++ sparklines.js
@@ -1,5 +1,28 @@
 /* Sparklines.js */
 
+//This prototype is provided by the Mozilla foundation and
+//is distributed under the MIT license.
+//http://www.ibiblio.org/pub/Linux/LICENSES/mit.license
+if (!Array.prototype.map)
+{
+  Array.prototype.map = function(fun /*, thisp*/)
+  {
+    var len = this.length;
+    if (typeof fun != "function")
+      throw new TypeError();
+
+    var res = new Array(len);
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++)
+    {
+      if (i in this)
+        res[i] = fun.call(thisp, this[i], i, this);
+    }
+
+    return res;
+  };
+}
+
 
 var BaseSparkline = function() {
   this.init = function(id,data,mixins) {
